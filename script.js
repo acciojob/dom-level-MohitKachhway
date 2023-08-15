e496f8666b58808c32fca8894954083d41c064d4
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-08-15 11:44:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let ul = document.querySelector("ul");
+let li = ul.children;
+let l = document.getElementById("level");
+let ind = Array.from(li).indexOf(l)
+alert(`The level of the element is: ${ind}`)
